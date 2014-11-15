839918b25dabc52a3496e34fd162b0582d17df04
Dan McKay <daniel.mckay@feedhenry.com>
2014-11-15 18:30:58 +0000

    added dummy credentials.js

---
+++ App/config/credentials.js
@@ -0,0 +1,7 @@
+var credentials = module.exports = {
+  username: "YOUR_MONGODB_USERNAME",
+  password: "YOUR_MONGODB_PASSWORD",
+  host: "YOUR_MONGODB_HOST",
+  port: "YOUR_MONGODB_PORT",
+  dbname: "YOUR_MONGODB_DATABASE_NAME"
+}
